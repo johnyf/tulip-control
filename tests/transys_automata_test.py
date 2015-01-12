b36a711b61af7128304d2b8ecccb971ada2a8fff
Ioannis Filippidis <jfilippidis@gmail.com>
2015-01-12 02:26:48 -0800

    update rabin_test to renaming: check_sanity -> is_consistent

--- tests/transys_automata_test.py
+++ tests/transys_automata_test.py
@@ -5,8 +5,7 @@ from tulip.transys import Automaton
 
 
 def rabin_test():
-    dra = trs.Automaton(acceptance='Rabin')
-    print(dra)
+    dra = Automaton(acceptance='Rabin')
 
     dra.add_nodes_from(xrange(10))
     dra.accepting_sets.append({'[]<>': {1, 2}, '<>[]!': {3, 4}})
@@ -14,9 +13,9 @@ def rabin_test():
     assert isinstance(dra.accepting_sets, list)
     assert dra.accepting_sets[0]['[]<>'] == {1, 2}
     assert dra.accepting_sets[0]['<>[]!'] == {3, 4}
-    assert dra.check_sanity()
+    assert dra.is_consistent()
 
-    dra = trs.Automaton(acceptance='Rabin')
+    dra = Automaton(acceptance='Rabin')
     dra.add_nodes_from(xrange(5))
     dra.accepting_sets.append({'[]<>': [1], '<>[]!': [2, 4]})
     dra.accepting_sets[0]['[]<>'].append(2)
@@ -29,7 +28,9 @@ def rabin_test():
     s = dra.accepting_sets[1]
     assert set(s['[]<>']) == {2, 1}
     assert set(s['<>[]!']) == set()
-    assert dra.check_sanity()
+    assert dra.is_consistent()
+
+
 def ba_test():
     ba = Automaton(acceptance='Buchi')
     ba.alphabet['p'] = 'boolean'
